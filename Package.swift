29282418ed1cdef12694bea2bd769a19c184d515
Aleix Guri <crisredfi1@gmail.com>
2016-11-08 10:30:11 +0100

    post and get requests working

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Instagram",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
-        .Package(url:"https://github.com/vapor/postgresql-provider", majorVersion:1, minor: 0)
+        .Package(url:"https://github.com/vapor/postgresql", majorVersion:1, minor: 0)
     ],
     exclude: [
         "Config",
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,10 +1,13 @@
 import Vapor
-import VaporPostgreSQL
+import PostgreSQL
+import HTTP
 
-let drop = Droplet(
-    providers: [VaporPostgreSQL.Provider.self]
+let drop = Droplet()
+let postgreSQL =  PostgreSQL.Database(
+    dbname: "Instagram",
+    user: "crisredfi",
+    password: ""
 )
-
 //drop.get { req in
 //    return try drop.view.make("welcome", [
 //    	"message": drop.localization[req.lang, "welcome", "title"]
@@ -13,12 +16,33 @@ let drop = Droplet(
 //
 //drop.resource("posts", PostController())
 drop.get("version") { request in
-    if let db = drop.database?.driver as? PostgreSQLDriver {
-        let version = try db.raw("SELECT version()")
-        return try JSON(node: version)
-    } else {
+    do {
+        //let version = try postgreSQL.execute("SELECT version()")
+        //return  version
+        let connection = try postgreSQL.makeConnection()
+        let results = try postgreSQL.execute("SELECT * FROM instagram_post")
+
+        print("\(results)")
+        return "hello world"
+    } catch {
         return "NO DB connection"
     }
 }
+
+drop.post("addPost") { request in
+
+    guard let name = request.data["name"]?.string else {
+        throw Abort.badRequest
+    }
+    do {
+        let connection = try postgreSQL.makeConnection()
+        let result = try postgreSQL.execute("Insert INTO instagram_post(name) VALUES('\(name)')")
+        return Response(status: .ok)
+    } catch {
+        throw Abort.serverError
+    }
+
+}
+
 
 drop.run()
